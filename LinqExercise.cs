195ecbb59544daa86551de6a539ced11a89fbc75
Vladimir <63316608+Tov-Vl@users.noreply.github.com>
2022-03-07 15:41:29 +0300

    Add files via upload

---
+++ LinqExercise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var arr = new[] { 1, 2, 4, 5, 7 };
+            var res = arr.Join(arr, i => i, i => i+1, (i, i2) => i+1);
+
+            Console.WriteLine(String.Join(", ", res));
+        }
+    }
+}
